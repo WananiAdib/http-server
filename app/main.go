81341c2036665192d046c0740d9e8d567ffb0fe1
Adib El Ounani <wananiadib@gmail.com>
2025-04-19 16:27:30 +0100

    goroutines ftw

--- app/main.go
+++ app/main.go
@@ -21,57 +21,63 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
 
-	buf := make([]byte, 1024)
-
-	reqLen, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
-	}
-
-	request := string(buf[:reqLen])
-
-	path := strings.Split(request, " ")
-
-	if path[1] == "/" {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error sending 200 request: ", err.Error())
-			os.Exit(1)
-		}
-	} else if strings.HasPrefix(path[1], "/echo") {
-		content := path[1][6:]
-		_, err = conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
-		if err != nil {
-			fmt.Println("Error sending 200 request: ", err.Error())
-			os.Exit(1)
-		}
-	} else if strings.HasPrefix(path[1], "/user-agent") {
-		lines := strings.Split(request, "\r\n")
-		i, err := findUserAgentLine(lines)
-		if err != nil {
-			fmt.Println("Error reading User-Agent ", err.Error())
-			os.Exit(1)
-		}
-		userAgent := strings.TrimSpace(lines[i][11:])
-		_, err = conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
-		if err != nil {
-			fmt.Println("Error sending 200 request: ", err.Error())
-			os.Exit(1)
-		}
-	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error sending 404 request: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
+		go func(conn net.Conn) {
+
+			buf := make([]byte, 1024)
+
+			reqLen, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading request: ", err.Error())
+				os.Exit(1)
+			}
+
+			request := string(buf[:reqLen])
+
+			path := strings.Split(request, " ")
+
+			if path[1] == "/" {
+				_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+				if err != nil {
+					fmt.Println("Error sending 200 request: ", err.Error())
+					os.Exit(1)
+				}
+			} else if strings.HasPrefix(path[1], "/echo") {
+				content := path[1][6:]
+				_, err = conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
+				if err != nil {
+					fmt.Println("Error sending 200 request: ", err.Error())
+					os.Exit(1)
+				}
+			} else if strings.HasPrefix(path[1], "/user-agent") {
+				lines := strings.Split(request, "\r\n")
+				i, err := findUserAgentLine(lines)
+				if err != nil {
+					fmt.Println("Error reading User-Agent ", err.Error())
+					os.Exit(1)
+				}
+				userAgent := strings.TrimSpace(lines[i][11:])
+				_, err = conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
+				if err != nil {
+					fmt.Println("Error sending 200 request: ", err.Error())
+					os.Exit(1)
+				}
+			} else {
+				_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				if err != nil {
+					fmt.Println("Error sending 404 request: ", err.Error())
+					os.Exit(1)
+				}
+
+			}
+		}(conn)
 	}
 
 }
